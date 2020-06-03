cbb67f96a18bdcd1b511ffbb8831800c6bd59a18
msisimko <65783003+msisimko@users.noreply.github.com>
2020-06-03 21:43:05 +0300

    Fixed Landing route error

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,7 +1,7 @@
 export const ACCOUNT = '/account';
 export const ADMINISTRATOR = '/administrator';
 export const HOME = '/home';
-export const LANDING = '/landing';
+export const LANDING = '/';
 export const PASSWORD_FORGET = '/pw-forget';
 export const SIGN_IN = '/sign-in';
 export const SIGN_UP = '/sign-up';
