c95b62f5e191dfadf1d972e98877cab202409a7a
Kevin <65783003+msisimko@users.noreply.github.com>
2020-06-09 19:45:15 +0300

    Authorization and Route Protection

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 
 import PasswordChange from '../../components/PasswordChange';
 
+import { withAuthorization } from '../../session';
+
 class Account extends Component {
   render() {
     return(
       <React.Fragment>
         <h1>Account</h1>
         <PasswordChange />
+        <p>This page is only accessible to logged in users.</p>
       </React.Fragment>
     );
   }
 }
 
-export default Account;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(Account);
--- src/pages/Administrator/index.js
+++ src/pages/Administrator/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
+import { withAuthorization } from '../../session';
+
 class Administrator extends Component {
   render() {
     return(
       <React.Fragment>
         <h1>Administrator</h1>
+        <p>This page is only accessible to logged in users.</p>
       </React.Fragment>
     );
   }
 }
 
-export default Administrator;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(Administrator);
