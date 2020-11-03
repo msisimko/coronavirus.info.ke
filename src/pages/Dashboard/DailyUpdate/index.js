364125fa6fa0b5badf8f1182e4d774a4bee3e569
Kevin <65783003+msisimko@users.noreply.github.com>
2020-11-03 12:02:30 +0300

    Redesigned Dashboard using Accordions

---
+++ src/pages/Dashboard/DailyUpdate/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+import { withStyles } from '@material-ui/core/styles';
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const styles = theme => ({
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+});
+
+class DailyUpdate extends Component {
+  render() {
+    const { classes } = this.props;
+
+    return(
+      <Accordion elevation={0} square>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1bh-content" id="panel1bh-header">
+          <Box p={1}>
+            <Typography className={classes.heading}>Daily Update</Typography>
+            <Typography className={classes.secondaryHeading}>What's the latest within the last 24 hours?</Typography>
+          </Box>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(DailyUpdate);
